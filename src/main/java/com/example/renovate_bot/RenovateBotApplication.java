8bc8b98a1379bdf269701220d72f38fa6a423a15
amanglani2503 <ambermanglani8871@gmail.com>
2026-04-05 15:37:31 +0530

    Initial commit with github setup

---
+++ src/main/java/com/example/renovate_bot/RenovateBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.renovate_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovateBotApplication.class, args);
+	}
+
+}
